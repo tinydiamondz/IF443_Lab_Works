a21792321ff89cb475b4ce5803bfd4b0653be6a1
Rap <tinydiamondzgt@gmail.com>
2026-02-07 20:57:04 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000130426_RafiAthallahAhmadHaryanto/week01/GameStore.kt
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week01/GameStore.kt
@@ -1,7 +1,16 @@
 package oop_00000130426_RafiAthallahAhmadHaryanto.week01
 
 fun main () {
-    val gameTitle: String
-    val price: Int
-
+    val gameTitle: String = "Call of Duty: Black Ops 7"
+    val price: Int = 1200000
+    calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int {
+    if(price > 50000) {
+           return price - (price * 20 / 100)
+    }else{
+        return price - (price * 10/100)
+    }
+}
+

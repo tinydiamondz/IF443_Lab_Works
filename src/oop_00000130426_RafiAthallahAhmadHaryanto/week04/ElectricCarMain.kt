21c5bd4ef5efc8a64a1cac42c16acc814b301288
Rap <tinydiamondzgt@gmail.com>
2026-02-27 20:02:37 +0700

    week04: (task 1) test ElectricCar in main

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week04/ElectricCarMain.kt
@@ -0,0 +1,8 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week04
+
+fun main(){
+    val ElectricCar = ElectricCar("BYD SEAL", 4, 100)
+    ElectricCar.accelerate();
+    ElectricCar.honk();
+    ElectricCar.openTrunk();
+}

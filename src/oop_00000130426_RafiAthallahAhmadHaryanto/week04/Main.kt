9c8d40d21aa7d57ea267aa4971a4bb10cddfbe5a
Rap <tinydiamondzgt@gmail.com>
2026-02-27 19:45:45 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}

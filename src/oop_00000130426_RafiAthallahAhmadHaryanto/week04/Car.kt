c0be5899f213f81d89f4be932b80cc753f389d65
Rap <tinydiamondzgt@gmail.com>
2026-02-27 17:26:34 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week04/Car.kt
@@ -0,0 +1,8 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week04
+
+// Car "Is-A" Vehicle. Parameter brand dilempar ke atas melalui Vehicle(brand)
+open class Car (brand: String, val numberOfDoors: Int): Vehicle(brand) {
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}

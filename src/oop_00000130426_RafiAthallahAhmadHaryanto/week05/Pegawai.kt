b895dfc4f8a5f0df97cfa7f5c2ffadc88b60a75b
Rap <tinydiamondzgt@gmail.com>
2026-03-06 10:10:40 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}

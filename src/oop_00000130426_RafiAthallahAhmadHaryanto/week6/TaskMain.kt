b7c1d6cd5f9ae3a4a676ecdf40fcd1b2fbcb2fc5
Rap <tinydiamondzgt@gmail.com>
2026-03-13 21:31:22 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week6/TaskMain.kt
@@ -0,0 +1,8 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week6
+
+fun main() {
+    val hub = SmartHomeHub()
+    hub.addDevice(SmartLamp("1", "Ruang Tamu"))
+    hub.addDevice(SmartSpeaker("1", "Google Nest Dapur"))
+    hub.addDevice(SmartCCTV("1", "Ezviz Garasi"))
+}

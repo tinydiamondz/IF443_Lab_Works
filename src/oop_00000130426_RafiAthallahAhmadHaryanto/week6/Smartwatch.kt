73d261a4583f7679856662c1561f935c74f34b54
rapharyanto <tinydiamondzgt@gmail.com>
2026-03-13 10:40:40 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week6/Smartwatch.kt
@@ -0,0 +1,15 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week6
+
+class Smartwatch: Watch(), BluetoothConnectable, Rechargeable {
+    override fun  showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}

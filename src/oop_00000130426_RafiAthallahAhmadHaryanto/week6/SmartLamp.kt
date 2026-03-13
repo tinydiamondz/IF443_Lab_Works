690f319a94ef4204f4d0d3cf1745451ef693ea96
Rap <tinydiamondzgt@gmail.com>
2026-03-13 15:28:18 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week6/SmartLamp.kt
@@ -0,0 +1,10 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week6
+
+class SmartLamp (override val id: String, override val name: String): SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Lamp has been turned on")
+    }
+    override fun turnOff() {
+        println("Lamp has been turned off")
+    }
+}

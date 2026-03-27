3a1eb8418249e03e87e9fbfdb52ed5a0d5902e07
Rap <tinydiamondzgt@gmail.com>
2026-03-27 16:28:05 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000130426_RafiAthallahAhmadHaryanto/week07/SystemStates.kt
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week07/SystemStates.kt
@@ -2,4 +2,10 @@ package oop_00000130426_RafiAthallahAhmadHaryanto.week07
 
 enum class Appstate {
     STARTING, RUNNING, STOPPED
-}`
+}
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}

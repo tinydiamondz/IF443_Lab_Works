f4a3a43b57927b0ae6e942b1b3fa86c0f43b653d
Rap <tinydiamondzgt@gmail.com>
2026-04-17 16:01:58 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
